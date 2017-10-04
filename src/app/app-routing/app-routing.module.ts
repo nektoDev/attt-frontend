359cbf521c1719d8581cdafc647cb98f375cbe3c
nektodev <ts.slawa@gmail.com>
2017-10-05 00:36:42 +0300

    Add dashboard component

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {AppComponent} from '../app.component';
+import {DashboardComponent} from '../dashboard/dashboard.component';
 
 const routes: Routes = [
   {
-    path: '', component: AppComponent
+    path: '', component: DashboardComponent
   },
 ];
 
