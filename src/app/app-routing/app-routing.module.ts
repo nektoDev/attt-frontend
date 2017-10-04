9114fc6724e54bfd5b1c7bcee476c321510b0c9d
nektodev <ts.slawa@gmail.com>
2017-10-05 00:43:08 +0300

    Add links between torrents and dashboard

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AppComponent} from '../app.component';
 import {DashboardComponent} from '../dashboard/dashboard.component';
+import {TorrentsComponent} from '../torrents/torrents.component';
 
 const routes: Routes = [
-  {
-    path: '', component: DashboardComponent
-  },
+  { path: '', component: DashboardComponent },
+  { path: 'torrents', component: TorrentsComponent },
 ];
 
 
