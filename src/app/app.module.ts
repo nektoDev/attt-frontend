87bc729b714fceda3744866dba1259156eeb356b
nektodev <ts.slawa@gmail.com>
2017-10-05 01:13:48 +0300

    Add toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AppRoutingModule} from './app-routing/app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TorrentsComponent } from './torrents/torrents.component';
+import {MdButtonModule, MdToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { TorrentsComponent } from './torrents/torrents.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdToolbarModule,
+    MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
